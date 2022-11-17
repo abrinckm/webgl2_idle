6149d051a8637fb0b62e7e6b2b530f7a2189453f
Adam Brinckman <abrinckm@gmail.com>
2022-11-17 13:49:38 -0500

    anim

--- idle/public/js/index.js
+++ idle/public/js/index.js
@@ -2,25 +2,20 @@ async function main() {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
 
-    // set line stroke and line width
-    ctx.strokeStyle = "rgb(200,0,0)";
-    ctx.fillStyle = "rgb(200, 0, 0)";
-    ctx.lineWidth = 1;
+    function flipBook() {
+        ctx.clearRect(0, 0, 1280, 720);
+        ctx.save()
+        ctx.translate(300, 300)
+        ctx.rotate(.001);
+        ctx.translate(-300, -300)
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        ctx.fillRect(100, 100, 500, 500);
+        // ctx.restore()
 
-    // draw a red triangle
-    // ctx.beginPath();
-    // ctx.moveTo(100, 300);
-    // ctx.lineTo(300, 300);
-    // ctx.lineTo(200, 200)
-    // ctx.lineTo(100, 300);
-    // ctx.fill();
+        window.requestAnimationFrame(flipBook);
+    }
 
-    // show alpha channel
-    ctx.fillStyle = "rgb(200, 0, 0)";
-    ctx.fillRect(100, 100, 500, 500);
-
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(300, 300, 500, 500);
+    window.requestAnimationFrame(flipBook);
 }
 
 
